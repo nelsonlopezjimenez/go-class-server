73cc2a7dfcdfae2634c7dd52ea45f0435137c751
rConnor <rConnor@MonkeyGoblin.death>
2026-03-26 18:40:42 -0700

    CHORE: Add build.sh and remove from .gitignore

---
+++ build.sh
@@ -0,0 +1,26 @@
+ECHO "checking for binary repo"
+if checkForBinRepo != 0; then echo "Failed to build binary"; exit 1;
+ECHO "Building the latest binary now";
+go build -v -o bin/classServer.exe;
+cd bin;
+ECHO $PWD;
+ECHO "Preparing to commit";
+git add classServer.exe;
+git commit -m "v2.3.0"
+ECHO "Preparing to push new binary to remote repo"
+ECHO "Pushing to origin"
+git push -f --progress origin development:main;
+# ECHO "Pushing to MonkeyGoblin"
+# git push -f --progress Monkey development:main
+
+function checkForBinRepo() {
+if ! test -d bin; 
+then 
+echo "WARN: No bin directory detected";
+echo "WARN: Please make sure you are connected to the network";
+echo "WARN: If there is no network, build will fail without bin directory";
+git clone http://192.168.1.28:3000/ClassroomResources/classServer.git bin;
+if [ $? != 0 ]; then echo "Cloning repo failed"; exit 1; fi;
+fi;
+
+}
